f0e9798bb198ffaf45c6e6c82c48f4bdb6dfb81a
Martin Zahradnik <zahradnik.admin@gmail.com>
2019-08-25 12:30:22 +0200

    create function for localization crrencies

--- client/src/localization/index.ts
+++ client/src/localization/index.ts
@@ -1,6 +1,22 @@
 import en from "localization/en/index";
 import de from "localization/de/index";
 
+export const localizePrice = (
+  price: number,
+  locale: string = "en-US",
+  currency: string = "USD"
+): string => {
+  if (price === 0) {
+    return "FREE";
+  }
+
+  return price.toLocaleString(locale, {
+    style: "currency",
+    currency,
+    maximumFractionDigits: 2
+  });
+};
+
 export type availableLocales = "en" | "de";
 
 export default {
