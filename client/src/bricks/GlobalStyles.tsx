098846598ed101f99aa57bd48ce46fb3130fde91
Martin Zahradnik <zahradnik.admin@gmail.com>
2019-08-25 12:27:47 +0200

    setup css reset, global styling and theming for app

--- client/src/bricks/GlobalStyles.tsx
+++ client/src/bricks/GlobalStyles.tsx
@@ -1,17 +1,40 @@
 import { createGlobalStyle } from "styled-components";
-import styledNormalize from "styled-normalize";
+import reset from "styled-reset";
 
 export default createGlobalStyle`
-  ${styledNormalize}
+  ${reset}
   
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap');
+
   html {
+    box-sizing: border-box;
     font-size: 62.5%;
+    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
   }
 
+
   body {
-    font-family: "Roboto", "Helvetica", "Arial", sans-serif;
-    color: blue;
+    color: #595959;
     font-size: 1.6rem;
+    
+  }
+
+  h1, h2, h3 {
+    text-align: center;
+  }
+
+
+  h3 {
+    font-size: 1.8rem;
+    margin-bottom: 1.5rem;
+  }
+
+  h4 {
+    font-size: 1.4rem; 
+    margin-bottom: 1.1rem;
   }
   
   a {
--- client/src/bricks/theme.tsx
+++ client/src/bricks/theme.tsx
@@ -1,4 +1,23 @@
 export default {
-  primary: "brown",
-  secondary: "green"
+  color: {
+    primary: "#8C7B6B",
+    secondary: "#D8C2AF",
+    background: "#FFFFFF",
+    subHeading: "#999999",
+    text: "#595959"
+  },
+  text: {
+    fontSize: {
+      small: "1.2rem",
+      normal: "1.4rem",
+      bigger: "1.8rem",
+      big: "2.6rem"
+    },
+    lineHeight: {
+      small: "1.8rem",
+      normal: "2.1rem",
+      bigger: "2.7rem",
+      big: "3.9rem"
+    }
+  }
 };
