1f6ba11ea0dd391d5956996232d27c1e18b8abfa
Martin Zahradnik <zahradnik.admin@gmail.com>
2019-10-29 10:43:34 +0100

    setup heroku deploy

--- server/app.ts
+++ server/app.ts
@@ -76,6 +76,14 @@ app.use(
   }
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 mongoose
   .connect(
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-dgbbx.mongodb.net/${process.env.DB_NAME}?retryWrites=true`
